36f6da66f985ae8e0dc69087186e2e1f47c2df0d
MuraFederico <federicomura98@gmail.com>
2022-06-28 14:16:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+int Power(int number)
+{
+    return number * number;
+};
+
